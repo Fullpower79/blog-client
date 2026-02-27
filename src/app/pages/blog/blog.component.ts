833d08200b3bc1521ef48acded06f2fc2aa0ace0
Fullpower79 <Cedricadriaenssens11@gmail.com>
2026-02-27 13:38:15 +0100

    nog een poging gedaan om de blogs te laden laden

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -15,20 +15,38 @@ export class BlogComponent implements OnInit {
 
   blogs: Blog[] = [];
   tagFilter = '';
+  loading = false;
+  errorMessage = '';
 
   constructor(private blogService: BlogService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    // try to fetch right away; if the HTTP client isn't ready this will fail silently,
+    // so we also expose a manual button below for users to retry.
     this.loadBlogs();
   }
 
   loadBlogs(): void {
+    // guard early; if user keeps clicking we don't want overlapping requests
+    if (this.loading) {
+      return;
+    }
+
+    this.loading = true;
+    console.log('loading blogs, tagFilter=', this.tagFilter);
+
     this.blogService.getBlogs(this.tagFilter).subscribe({
       next: data => {
         this.blogs = data;
+        this.loading = false;
         this.cdr.detectChanges();
       },
-      error: err => console.error('Error loading blogs', err)
+      error: err => {
+        console.error('Error loading blogs', err);
+        this.errorMessage = 'Failed to load blogs. Please check your connection or try again.';
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
     });
   }
 
